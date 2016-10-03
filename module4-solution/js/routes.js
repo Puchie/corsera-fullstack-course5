c878845a680af971271eaef9a0663b57098b4e0d
rip <developer@croakapps.com>
2016-10-03 22:03:24 +0800

    non working; home view and category view

---
+++ module4-solution/js/routes.js
@@ -0,0 +1,25 @@
+(function() {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/MenuApp/templates/home.template.html'
+  })
+  .state('categories', {
+    url: 'categories',
+    template: '<h2>Here I am</h2>'
+    // templateUrl: 'src/MenuApp/templates/categories.template.html'
+  })
+  ;
+
+}
+})();
